0a249704a26686b9c3d2fb7b452cea8a58ef7f0d
jxnata <jxnata@proton.me>
2024-07-20 20:32:39 -0300

    feat: ✨ google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import App from './App.tsx'
 import './index.css'
 
+ReactGA.initialize('G-PFME64BT79')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 )
